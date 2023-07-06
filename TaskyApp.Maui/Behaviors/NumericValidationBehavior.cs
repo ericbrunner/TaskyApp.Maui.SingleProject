8c89460a9d5af22023140f7be7881e6e4224b90e
Erich Brunner <e.brunner@msoftorga.onmicrosoft.com>
2023-07-06 08:07:12 +0200

    migrated a Xamarin.Forms Behavior

---
+++ TaskyApp.Maui/Behaviors/NumericValidationBehavior.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskyApp.Maui.SingleProject.Behaviors;
+
+public class NumericValidationBehavior : Behavior<Entry>
+{
+    protected override void OnAttachedTo(Entry entry)
+    {
+        entry.TextChanged += EntryOnTextChanged;
+        base.OnAttachedTo(entry);
+    }
+
+    private void EntryOnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var isValid = double.TryParse(e.NewTextValue, out _);
+        ((Entry)sender).TextColor = isValid ? Colors.Green : Colors.Red; 
+    }
+}
